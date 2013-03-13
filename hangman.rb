355a1c651c8571086f16a39cc57ade98f4e0a63c
Tommy Kail <tommy.kail@icloud.com>
2013-03-13 18:25:34 -0400

    Initial commit

---
+++ hangman.rb
@@ -0,0 +1,64 @@
+class Hangman
+	@tries_left = 8
+	@blank = ""
+
+	# def self.tries_left=(num)
+	# 	@tries_left = num
+	# end
+
+	def self.tries_left
+		@tries_left
+	end
+	
+	def self.select_random_word
+		dictionary = ["ruby", "rails", "monk", "monkey", "ridiculous", "luscious", "moist", "asscrack"]
+		number = rand(3)
+		dictionary[number]
+	end
+
+	def self.make_blank(word)
+		@blank = "_" * word.length
+		@blank
+	end
+
+	def self.letter_check(letter, word)
+		word.include?(letter)
+	end
+
+	def self.outcome(result)
+		if result
+			# we'll put it here
+			return "You guessed correctly!"
+		else
+			@tries_left -= 1
+			return "You have #{@tries_left} tries left."
+		end
+	end
+
+	def self.find_letter_positions(guessed_letter, word)
+		index_array = []
+		word.split("").each_with_index { |letter, index| index_array << index if letter == guessed_letter }
+		index_array
+	end
+
+	def self.put_letters_into_blank(blank, guessed_letter, index_array)
+		blank = blank.split("") 
+		(0...blank.length).each do |blank_index|
+			if index_array.include?(blank_index)
+				blank[blank_index] = guessed_letter
+			end
+		end
+		blank.join("")
+	end
+
+	def self.new_blank(guessed_letter, word, blank)
+		index_array = self.find_letter_positions(guessed_letter, word)
+		self.put_letters_into_blank(blank, guessed_letter, index_array)
+	end
+
+	
+
+# to do:
+# • change from self to Hangman.new
+
+end
---
+++ hangman_test.rb
@@ -0,0 +1,38 @@
+require "test/unit"
+require_relative "hangman"
+
+class HangmanTest < Test::Unit::TestCase
+
+	def test_select_word
+		dictionary = ["ruby", "rails", "monk", "monkey", "ridiculous", "luscious", "moist", "asscrack"]
+		assert_equal true, dictionary.include?(Hangman.select_random_word)
+	end
+
+	def test_blank_word
+		assert_equal "______", Hangman.make_blank("string")
+	end
+
+	def test_guess_letter
+		assert_equal true, Hangman.letter_check("t", "train")
+		assert_equal false, Hangman.letter_check("z", "brain")
+	end
+
+	def test_answer
+		Hangman.outcome(false)
+		assert_equal "You have 7 tries left.",  "You have #{Hangman.tries_left} tries left."
+		assert_equal "You guessed correctly!", Hangman.outcome(true)
+	end
+
+	def test_if_correct_letter_positions_found
+		assert_equal [3, 6], Hangman.find_letter_positions("c", "asscrack")
+		assert_equal [], Hangman.find_letter_positions("b", "montreal")
+	end
+
+	def test_if_blanks_are_filled_with_letters
+		assert_equal "_ll_", Hangman.put_letters_into_blank("____", "l", [1, 2])
+	end
+
+	def test_if_blank_renews
+		assert_equal "_ll__", Hangman.new_blank("l", "allow", "_____")
+	end
+end
