4a4ec4e5fc5d312d68664f4922233c62da3c06d7
Matthew <mattvleming@gmail.com>
2013-03-14 10:31:01 -0400

    Improve method names

--- hangman_test.rb
+++ hangman_test.rb
@@ -5,16 +5,16 @@ class HangmanTest < Test::Unit::TestCase
 
 	def test_select_word
 		dictionary = ["ruby", "rails", "monk", "monkey", "ridiculous", "luscious", "moist", "asscrack"]
-		assert_equal true, dictionary.include?(Hangman.select_random_word)
+		assert_equal true, dictionary.include?(Hangman.get_word)
 	end
 
 	def test_blank_word
-		assert_equal "______", Hangman.make_blank("string")
+		assert_equal "______", Hangman.make_word_blank("string")
 	end
 
 	def test_guess_letter
-		assert_equal true, Hangman.letter_check("t", "train")
-		assert_equal false, Hangman.letter_check("z", "brain")
+		assert_equal true, Hangman.check_word_has_letter("t", "train")
+		assert_equal false, Hangman.check_word_has_letter("z", "brain")
 	end
 
 	def test_answer
@@ -24,29 +24,29 @@ class HangmanTest < Test::Unit::TestCase
 	end
 
 	def test_if_correct_letter_positions_found
-		assert_equal [3, 6], Hangman.find_letter_positions("c", "asscrack")
-		assert_equal [], Hangman.find_letter_positions("b", "montreal")
+		assert_equal [3, 6], Hangman.find_where_letter_is_in_word("c", "asscrack")
+		assert_equal [], Hangman.find_where_letter_is_in_word("b", "montreal")
 	end
 
 	def test_if_blanks_are_filled_with_letters
 		Hangman.blank = "_____"
-		assert_equal "_ll__", Hangman.put_letters_into_blank("l", [1, 2])
+		assert_equal "_ll__", Hangman.put_letter_into_blank_where_it_was_in_word("l", [1, 2])
 	end
 
 	def test_if_blank_renews
 		Hangman.blank = "_____"
-		assert_equal "_ll__", Hangman.new_blank("l", "allow")
+		assert_equal "_ll__", Hangman.put_letter_into_blank("l", "allow")
 	end
 
 	def test_if_tries_run_out
 		Hangman.tries_left = 0
-		Hangman.lost?(Hangman.tries_left)
+		Hangman.game_lost?(Hangman.tries_left)
 		assert_equal true, Hangman.game_lost
 	end
 
 	def test_if_full_word_is_guessed
 		Hangman.blank = "motorcycle"
-		Hangman.won?(Hangman.blank)
+		Hangman.game_won?(Hangman.blank)
 		assert_equal true, Hangman.game_won
 	end
 end
